2073f9c96a58f9053f57e1ba02de4dde1036f5bb
juan <>
2024-04-30 16:48:35 -0300

    Update requests.get() to disable SSL verification

--- CERDownloaderPostgres.py
+++ CERDownloaderPostgres.py
@@ -19,7 +19,7 @@ def downloadCER():
 
     # Download the XLS file from the URL 
     try:
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
         response.raise_for_status()  # Check if the request was successful
         with open(file_path, "wb") as file:
             file.write(response.content)
