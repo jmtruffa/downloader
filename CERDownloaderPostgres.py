c17c8c377de464cdb662327738b0bfa4a1adc5b3
juan <>
2024-04-30 17:03:27 -0300

    Commented out unnecessary commit statement

--- CERDownloaderPostgres.py
+++ CERDownloaderPostgres.py
@@ -68,7 +68,7 @@ def downloadCER():
         # use Date type for the 'date' column in the database to get rid of the time part
         dtypeMap = {'date': sqlalchemy.types.Date}
         result = data_to_insert.to_sql(name = 'CER', con = db.engine, if_exists = 'append', index = False, dtype=dtypeMap, schema = 'public')
-        db.conn.commit()
+        #db.conn.commit()
         print(f"Number of records inserted as reported by the postgres server: {result}") 
     
     db.disconnect()
